89200209dbafef1678410a0e503acd999c8232ac
NunooFerreira <boostedlee@gmail.com>
2025-05-04 12:33:39 +0000

    comment changes

--- automation/attack/yo-yoattack.py
+++ automation/attack/yo-yoattack.py
@@ -7,7 +7,7 @@ from datetime import datetime
 TARGET_URL = "http://knative-fn4.default.127.0.0.1.nip.io/fib"
 NORMAL_CONCURRENCY = 0
 ATTACK_CONCURRENCY = 265            # Pod da scale aos 51, mas aumentar este valor mais so para ter a certez que da scale up 
-ON_ATTACK_DURATION = 35             # 120 para testar agora. 160 segundos foi o valor maximo ate um Pod começar a terminar antes do ataque terminar.
+ON_ATTACK_DURATION = 35             # 160 segundos foi o valor maximo ate um Pod começar a terminar antes do ataque terminar.
 OFF_ATTACK_DURATION = 900           # 15 minutos updated minutos de pausa 
 RUN_DURATION = 12 * 60 * 60         # Total run time in seconds (12 hours)
 CONNECTION_TIMEOUT = aiohttp.ClientTimeout(total=10)
